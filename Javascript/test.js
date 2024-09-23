e99743571492b552ad2ff612a04c6373cc1f6b6e
ARJENDRAGAUTAM <167452712+ARJENDRAGAUTAM@users.noreply.github.com>
2024-09-23 03:26:50 +0000

    container config & test environment

---
+++ Javascript/test.js
@@ -0,0 +1,11 @@
+console.log("hello arjendra");
+let str = "arjendra";
+if((str[0]=== "a") && (str.length > 3)){
+    console.log("This is a Good string!")
+}
+ else if(str === ""){
+    console.log("Please enter your string")
+}
+else{
+    console.log("This is not Good string")
+}
