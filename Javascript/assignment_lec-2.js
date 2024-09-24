9f3e3b5dcfb0850d0546c8cece97d17a63fa2f46
ARJENDRA SINGH <167452712+ARJENDRAGAUTAM@users.noreply.github.com>
2024-09-24 03:14:02 +0000

    done

---
+++ Javascript/assignment_lec-2.js
@@ -0,0 +1,69 @@
+
+// Question-1
+// let num = 14;
+// if (num % 10 === 0) {
+//     console.log("good");
+// } else {
+//     console.log("bad")
+
+// }
+//Q-2
+// let name = prompt("Enter Your name:")
+// let age = prompt("Enter Your age:"
+// // alert(`${name} is ${age} years old!`)
+// // Q-3
+// let months = 2;
+// switch (months) {
+//     case 1:
+//         console.log("January,February,March");
+//         break;
+//     case 2:
+//         console.log("April,May,June");
+//         break;
+//     case 3:
+//         console.log("july,August,September")
+//         break;
+//     case 4:
+//         console.log("October,November,December")
+//         break;
+//     default:
+//         console.log("nothing!")
+
+
+
+//Q-4
+// let str = "mAei Hello";
+// if((str[0] === "A") ||((str[0] === "a")) && (str.length > 5 )){
+//     console.log("this is golden string!");
+// }
+// else{
+//     console.log("This is not golden string!")
+// }
+// Q-5
+// let a = 101;
+// let b = 32;
+// let c = 4;
+// if(a > b){
+//     if(a > c)
+//     console.log("a is largest number!")
+// else{
+//     console.log("c is largest number")
+// }
+// } 
+// else{
+//     if(b > c){
+//         console.log("b is largest")
+//     }
+//     else{
+//         console.log("c is largest number")
+//     }
+// }
+// Q-6
+let num1=32;
+let num2=47855;
+if((num1%10) == (num2%10)){
+    console.log("numbers have the same last digit which is:", num1%10);
+}
+else{
+    console.log("numbers do not have the same last digit")
+}
