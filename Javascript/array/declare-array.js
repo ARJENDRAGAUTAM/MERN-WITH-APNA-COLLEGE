0185e6df4caef3a27ddd74dc11c42fb0f303df1d
ARJENDRA SINGH <167452712+ARJENDRAGAUTAM@users.noreply.github.com>
2024-09-24 15:31:57 +0000

    done

---
+++ Javascript/array/declare-array.js
@@ -0,0 +1,3 @@
+let arr = ["abc","b","c"];
+let check = arr[0];
+console.log(check);
---
+++ Javascript/slice.js
@@ -0,0 +1,7 @@
+// let msg = "Hello";
+// console.log(msg.slice(1));
+// // expected output : ello
+let Str = "Hey!arjendr";
+let newStr = Str.slice(-1);
+console.log(newStr);
+//expected output:r;
