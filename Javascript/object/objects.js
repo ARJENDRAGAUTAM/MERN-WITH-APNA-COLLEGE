5b43b43cf678e4b63a124ad59d0e4af7419d78ed
ARJENDRA SINGH <167452712+ARJENDRAGAUTAM@users.noreply.github.com>
2024-10-04 18:13:24 +0000

    done

---
+++ Javascript/object/objects.js
@@ -0,0 +1,16 @@
+const student = {
+    name : "arjendra",
+    age : 23,
+    marks : 80,
+    city : "Gurgaon",
+};
+// console.log(student);
+const post = {
+    username : "ArjendraSingh",
+    content : "This is my first post #hello_nothing",
+    likes : 156,
+    reposts : 7,
+    tags : "@aka,@kaushal,@satya",
+}
+console.log(post);
+console.log(post["likes"]);
