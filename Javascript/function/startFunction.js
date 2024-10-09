4add4286d19d896b682435104fb2c962b6e78a0d
ARJENDRA SINGH <167452712+ARJENDRAGAUTAM@users.noreply.github.com>
2024-10-09 04:06:55 +0000

    done

--- Javascript/function/startFunction.js
+++ Javascript/function/startFunction.js
@@ -21,8 +21,27 @@
 // checkAdult();
 
 // Practice-1- Create a function to roll dice & always display the value of the dice (1 -6);
-function dice(){
-    let test = Math.floor(Math.random()*6)  +1 ;
-    console.log(test);
+// function dice(){
+//     let test = Math.floor(Math.random()*6)  +1 ;
+//     console.log(test);
+// }
+// dice()
+
+//function with argument
+// function printInfo(name,age){
+//     console.log(`${name}'s is ${age}`);
+// }
+// printInfo("arjendra",23);
+// Practice Q-2- create a function that gives us the average of 3 numbers.
+// function avg(a,b,c){
+//     console.log("this is a average number",(a+b+c)/3);
+// }
+// avg(5,9,55);
+
+// Practice Q-3- Create a function that prints the multiplication table of a number.
+function table(num){
+    for(let i = num; i <= num*10;i+=num){
+        console.log(i);
+    }
 }
-dice()
+table(17);
