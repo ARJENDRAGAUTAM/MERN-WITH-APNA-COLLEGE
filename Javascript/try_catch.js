4e80cebc836e3c05630301586ece260972d3aa50
ARJENDRA SINGH <167452712+ARJENDRAGAUTAM@users.noreply.github.com>
2024-10-20 17:50:58 +0000

    done

---
+++ Javascript/try_catch.js
@@ -0,0 +1,11 @@
+console.log("hello");
+console.log("hello");
+try{
+    console.log(a);
+} catch(err){
+    console.log("cought an error... a is not  defined");
+    console.log(err)
+}
+console.log("hello2")
+console.log("hello2")
+console.log("hello2")
